bceadde0900fcd3aab4cc4e1e5538055c49a01e0
tItAnIc-45 <liamalpartida7@gmail.com>
2026-09-03 14:42:06 -0500

    Avance_pag_39

--- app/src/main/java/com/senati/appmantenimientoalumnos/NuevoActivity.java
+++ app/src/main/java/com/senati/appmantenimientoalumnos/NuevoActivity.java
@@ -1,6 +1,10 @@
 package com.senati.appmantenimientoalumnos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,55 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.senati.appmantenimientoalumnos.db.Dbalumnos;
+
 public class NuevoActivity extends AppCompatActivity {
+    //Asignamos nuestras variables
+    private EditText txtnombre, txtTelefono, txtCorreoElectronico;
+    private Button btnGuarda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nuevo);
+
+        //añadiendo las variables los elementos de la vista.
+        txtnombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreoElectronico = findViewById(R.id.txtCorreoElectronico);
+        btnGuarda = findViewById(R.id.BtnGuarda);
+
+        btnGuarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //cambiar el nombre de la clase alumnos por Dbalumnos
+                Dbalumnos dbalumnos = new Dbalumnos(NuevoActivity.this);
+
+                long id = dbalumnos.insertarContactos(
+                        txtnombre.getText().toString(),
+                        txtTelefono.getText().toString(),
+                        txtCorreoElectronico.getText().toString());
+
+                if (id > 0) {
+                    Toast.makeText(NuevoActivity.this, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show();
+                    Limpiar();
+                } else {
+                    Toast.makeText(NuevoActivity.this, "ERROR AL GUARDAR REGISTRO", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void Limpiar() {
+        txtnombre.setText("");
+        txtTelefono.setText("");
+        txtCorreoElectronico.setText("");
     }
 }
